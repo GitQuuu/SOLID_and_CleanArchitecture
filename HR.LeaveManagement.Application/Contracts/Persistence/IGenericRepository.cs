7c26ec7a5b7cbd96ece0d56c503a1d9edece4f7e
Qu <qu@peer-performance.com>
2025-02-11 10:57:01 +0100

    add basic function to generic repo contract

--- HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -2,5 +2,24 @@
 
 public interface IGenericRepository<T> where T : class
 {
+    Task<IEnumerable<T>> GetAsync();
+    Task<T?> GetByIdAsync(int id);
+    Task<T> CreateAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+}
+
+public interface IGenericRepository<T> where T : class
+{
+    
+}
+
+public interface IGenericRepository<T> where T : class
+{
+    
+}
+
+public interface IGenericRepository<T> where T : class
+{
     
 }
