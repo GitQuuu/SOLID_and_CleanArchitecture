ef6a5f5d04e77c840c8feda87db735b22e87ec99
Qu <qu@peer-performance.com>
2025-02-12 00:15:18 +0100

    22.Part 2

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+
+public class GetLeaveTypeDetailsHandler : IRequestHandler<GetLeaveTypeDetailsQuery, LeaveTypeDetailsDto>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+
+    public GetLeaveTypeDetailsHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        _mapper = mapper;
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+    
+    public async Task<LeaveTypeDetailsDto> Handle(GetLeaveTypeDetailsQuery request, CancellationToken cancellationToken)
+    {
+     var leaveTypeDetails = await _leaveTypeRepository.GetByIdAsync(request.Id);
+     var dto = _mapper.Map<LeaveTypeDetailsDto>(leaveTypeDetails);
+     return dto;
+    }
+}
