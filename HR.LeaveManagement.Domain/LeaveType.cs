e577b6a674cf7e0ebf4458985d113e2dbd20dbef
Qu <qu@peer-performance.com>
2025-02-11 09:03:08 +0100

    14.Creating the models for the core

---
+++ HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,22 @@
+﻿namespace HR.LeaveManagement.Domain;
+
+public class LeaveType
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int DefaultDays { get; set; }
+}
+
+public class LeaveRequest
+{
+    public int Id { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public LeaveType? LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    public DateTime DateRequested { get; set; }
+    public string? RequestComments  { get; set; }
+    public bool Approved { get; set; }
+    public bool Cancelled { get; set; }
+    public string? RequestingEmployeeId { get; set; }
+}
