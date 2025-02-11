590fb38226a2bb63745b8784203e0ee15e1297e0
Qu <qu@peer-performance.com>
2025-02-11 10:54:05 +0100

    Create a generic repository contract that takes in a parameter T

--- HR.LeaveManagement.Application/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace HR.LeaveManagement.Application;
-
-public class Class1
-{
-}
---
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace HR.LeaveManagement.Application.Contracts.Persistence;
+
+public interface IGenericRepository<T> where T : class
+{
+    
+}
