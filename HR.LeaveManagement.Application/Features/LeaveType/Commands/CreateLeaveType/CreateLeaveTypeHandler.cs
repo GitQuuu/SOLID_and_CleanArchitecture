c6c92d9c01aa47c7c78464e68c871ddeef13616a
Qu <qu@peer-performance.com>
2025-02-12 00:49:05 +0100

    23.Creatomg CQRS Command request and handler part 1

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+
+public class CreateLeaveTypeHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public CreateLeaveTypeHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        _mapper = mapper;
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+    public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+        // Validate incoming request
+        
+        // Convert to domain entity object 
+        var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+        
+        // add to db
+        
+        await _leaveTypeRepository.CreateAsync(leaveTypeToCreate);
+        
+        // return id
+        return leaveTypeToCreate.Id;
+    }
+}
