0b0e17778a2dd1bc3d28d9740312a23636140474
Qu <qu@peer-performance.com>
2025-02-12 11:01:04 +0100

    override the SaveChanges to automatically handle the DateModified and DateCreated

--- HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
+++ HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Domain.Common;
 using Microsoft.EntityFrameworkCore;
 
 namespace HR.LeaveManagement.Persistence.DatabaseContext;
@@ -13,4 +14,24 @@ public class HrDatabaseContext : DbContext
     public DbSet<LeaveType> LeaveTypes { get; set; }
     public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
     public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(HrDatabaseContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entry in base.ChangeTracker.Entries<BaseEntity>()
+                 .Where(q => q.State is EntityState.Added or EntityState.Modified))
+        {
+            entry.Entity.DateModified = DateTime.UtcNow;
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.DateCreated = DateTime.UtcNow;
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }
