3c5f0a4cd131a3b8d6ecf383aa5ca516346c0efb
Qu <qu@peer-performance.com>
2025-02-12 01:23:02 +0100

    24. Part 2

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType;
+
+public class DeleteLeaveTypeHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public DeleteLeaveTypeHandler(ILeaveTypeRepository leaveTypeRepository)
+    {
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+    
+    public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+        var leaveTypeToDelete = await _leaveTypeRepository.GetByIdAsync(request.Id);
+        if (leaveTypeToDelete is not null)
+        {
+            await _leaveTypeRepository.DeleteAsync(leaveTypeToDelete);
+        }
+        
+        return Unit.Value;
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType;
+
+public class UpdateLeaveTypeHandler : IRequestHandler<UpdateLeaveTypeCommand, Unit>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public UpdateLeaveTypeHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        _mapper = mapper;
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+    
+    public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+       var leaveTypeToUpdate = _mapper.Map<Domain.LeaveType>(request);
+       await _leaveTypeRepository.UpdateAsync(leaveTypeToUpdate);
+       
+       return Unit.Value;
+    }
+}
