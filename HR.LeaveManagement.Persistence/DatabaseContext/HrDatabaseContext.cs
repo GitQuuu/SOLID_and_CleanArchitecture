213d73bd3c0b19b2318bd91625cb899239546e83
Qu <qu@peer-performance.com>
2025-02-12 11:10:23 +0100

    move the seed data  / configuration its own class to utilize the one liner scan

--- HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
+++ HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
@@ -17,18 +17,9 @@ public class HrDatabaseContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // one liner to scan all configuration in the assembly
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(HrDatabaseContext).Assembly);
 
-        modelBuilder.Entity<LeaveType>().HasData(
-            new LeaveType
-            {
-                Id = 1,
-                Name = "Vacation",
-                DefaultDays = 10,
-                DateCreated = DateTime.UtcNow,
-                DateModified = DateTime.UtcNow,
-            }
-        );
         base.OnModelCreating(modelBuilder);
     }
 
