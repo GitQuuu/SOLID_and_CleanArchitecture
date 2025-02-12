beb12d94d8b3f4a3c5b414c53b6eaeb4ce1d1dc3
Qu <qu@peer-performance.com>
2025-02-12 13:43:25 +0100

    33.Add Repository implementation

--- HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -2,9 +2,9 @@
 
 public interface IGenericRepository<T> where T : class
 {
-    Task<ICollection<T>> GetAsync();
+    Task<IReadOnlyCollection<T>> GetAsync();
     Task<T?> GetByIdAsync(int id);
-    Task<T> CreateAsync(T entity);
-    Task<T> UpdateAsync(T entity);
-    Task<T> DeleteAsync(T entity);
+    Task CreateAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
 }
