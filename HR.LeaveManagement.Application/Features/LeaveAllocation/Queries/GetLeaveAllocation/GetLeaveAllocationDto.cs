34c7ac32afee8bc26900daae5448756186f4b388
Qu <qu@peer-performance.com>
2025-02-13 22:37:08 +0100

    working on get and get all for leaveAllocations feature

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocation/GetLeaveAllocationDto.cs
@@ -0,0 +1,16 @@
+﻿namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocation;
+
+public class GetLeaveAllocationDto
+{
+    public int NumberOfDays { get; set; }
+    public GetLeaveAllocationLeaveTypeDto? LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    public int Period { get; set; }
+    public string EmployeeId { get; set; } = string.Empty;
+}
+
+public class GetLeaveAllocationLeaveTypeDto
+{
+    public string Name { get; set; } = string.Empty;
+    public int DefaultDays { get; set; }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocation/GetLeaveAllocationHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocation;
+
+public class GetLeaveAllocationHandler : IRequestHandler<GetLeaveAllocationQuery, GetLeaveAllocationDto>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+
+    public GetLeaveAllocationHandler(IMapper mapper, ILeaveAllocationRepository leaveAllocationRepository)
+    {
+        _mapper = mapper;
+        _leaveAllocationRepository = leaveAllocationRepository;
+    }
+    
+    public async Task<GetLeaveAllocationDto> Handle(GetLeaveAllocationQuery request, CancellationToken cancellationToken)
+    {
+        var validator = new GetLeaveAllocationValidator();
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+        
+        var leaveAllocation = await _leaveAllocationRepository.GetByIdAsync(request.Id);
+        if (leaveAllocation is null)
+        {
+            throw new NotFoundException("Leave allocation not found.", nameof(request.Id));
+        }
+
+        return _mapper.Map<GetLeaveAllocationDto>(leaveAllocation);
+        
+    }
+}
