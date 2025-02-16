da9f2e3ee0e2b9b47ec76357191c38dc9bbafe8f
Qu <qu@peer-performance.com>
2025-02-16 21:52:37 +0100

    Generate JwtSecurityToken

--- HR.LeaveMangement.Identity/Services/AuthService.cs
+++ HR.LeaveMangement.Identity/Services/AuthService.cs
@@ -1,9 +1,12 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using HR.LeaveManagement.Application.Contracts.Identity;
 using HR.LeaveManagement.Application.Exceptions;
 using HR.LeaveManagement.Application.Models.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HR.LeaveMangement.Identity.Services;
 
@@ -39,7 +42,32 @@ public class AuthService : IAuthService
 
     private async Task<JwtSecurityToken> GenerateToken(User user)   
     {
+        var userClaims = await _userManager.GetClaimsAsync(user);
+        var roles = await _userManager.GetRolesAsync(user);
+        var roleClaims = roles.Select(q => new Claim(ClaimTypes.Role, q)).ToList();
+
+        var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.AspNetUser.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.AspNetUser.Email),
+                new Claim("uid", user.AspNetUser.Id),
+            }
+            .Union(userClaims)
+            .Union(roleClaims);
         
+        var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+        var signInCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+        var jwtSecurityToken = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(_jwtSettings.DurationInMinutes),
+            signingCredentials: signInCredentials
+        );
+        
+        return jwtSecurityToken;
     }
 
     public Task<RegistrationResponse> Register(RegistrationRequest request)
